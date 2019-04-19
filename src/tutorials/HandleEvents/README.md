d516044f329b58054b8b068731ca116d58b9312f
cra2ycoder <mohaa.r.6789@gmail.com>
2019-04-19 21:24:52 +0530

    handle events added

---
+++ src/tutorials/HandleEvents/README.md
@@ -0,0 +1,6 @@
+# Handling Events
+
+- react handle events == DOM events
+- only two differences
+  - event names are in `camelCase`
+  - Uses function name instead of `string` function name
---
+++ src/tutorials/HandleEvents/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class HandleEvents extends React.Component {
+  render() {
+    return ''
+  }
+}
+
+export { HandleEvents }
+export default HandleEvents
