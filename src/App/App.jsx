554e96a9e7a01603e1a89dce93bda57e2cc48577
cra2ycoder <mohaa.r.6789@gmail.com>
2019-04-20 22:26:41 +0530

    all folders moved to basics

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 // import '../tutorials'
-import '../examples'
+// import '../examples'
 
 class App extends React.Component {
   static defaultProps = {}
