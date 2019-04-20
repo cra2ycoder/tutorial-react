9e8b0fcefd57cdb471f7d0685e768d4f724f796f
cra2ycoder <mohaa.r.6789@gmail.com>
2019-04-20 18:44:51 +0530

    moved files to folder

--- src/tutorials/LiftUpState/index.js
+++ src/tutorials/LiftUpState/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import TimeCalculator from './TimeCalculator'
+import TimeCalculator from './TimeConversion/TimeCalculator'
 
 ReactDOM.render(<TimeCalculator />, document.getElementById('lift-up-state'))
