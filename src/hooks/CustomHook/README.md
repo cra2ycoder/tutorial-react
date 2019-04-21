99ec24487a469898b2c6991d925e0db47f62bd44
cra2ycoder <mohaa.r.6789@gmail.com>
2019-04-22 00:25:05 +0530

    custom hook added

--- src/hooks/CustomHook/README.md
+++ src/hooks/CustomHook/README.md
@@ -4,3 +4,45 @@
 - making re-usable function components with independent state handling
 - returning the state to another components as a value
 - passing as a argument to one component to another component
+
+## Rules to be followed
+
+- `use` word should be used as `prefix` of the function that we are declaring for reusable
+- as hooks looking for that one to consider as custom function
+
+## example
+
+```jsx
+function useFromInput(initialValue) {
+  // state
+  const [value, setValue] = useState(initialValue)
+
+  // handle change
+  function handleChange(e) {
+    setValue(e.target.value)
+  }
+
+  return {
+    value,
+    onClick: handleChange,
+  }
+}
+
+// usage
+const firstName = useFromInput('john')
+const lastName = useFromInput('doe')
+
+// placing with the dom
+<form>
+    <input
+        name="firstName"
+        placeholder="Enter your first name"
+        onChange={firstName.onClick}
+    />
+    <input
+        name="lastName"
+        placeholder="Enter your last name"
+        onChange={lastName.onClick}
+    />
+</form>
+```
--- src/hooks/CustomHook/index.js
+++ src/hooks/CustomHook/index.js
@@ -1,4 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
+import Row from '../../components/Row'
+import WindowResize from '../EffectHook/WindowResize'
 
-ReactDOM.render(<></>, document.getElementById('custom-hook'))
+/**
+ * @description
+ * custom hook method
+ * @param {*} initialValue
+ *
+ * used to handle the multiple user inputs
+ */
+function useFromInput(initialValue) {
+  // state
+  const [value, setValue] = useState(initialValue)
+
+  // handle change
+  function handleChange(e) {
+    setValue(e.target.value)
+  }
+
+  return {
+    value,
+    onClick: handleChange,
+  }
+}
+
+function UserForm() {
+  const firstName = useFromInput('john')
+  const lastName = useFromInput('doe')
+  return (
+    <>
+      <form>
+        <input
+          name="firstName"
+          placeholder="Enter your first name"
+          onChange={firstName.onClick}
+        />
+        <input
+          name="lastName"
+          placeholder="Enter your last name"
+          onChange={lastName.onClick}
+        />
+      </form>
+      <Row name="User Data">
+        <h3>{firstName.value}</h3>
+        <h3>{lastName.value}</h3>
+      </Row>
+      <WindowResize />
+    </>
+  )
+}
+
+ReactDOM.render(<UserForm />, document.getElementById('custom-hook'))
