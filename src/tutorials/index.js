834a3ab5d53684235bd023a7492a5238f201a571
cra2ycoder <mohaa.r.6789@gmail.com>
2019-04-25 17:27:37 +0530

    imports enabled

--- src/tutorials/index.js
+++ src/tutorials/index.js
@@ -1,25 +1,25 @@
 /**
  * Basics
  */
-// export * from './basics/HelloWorld'
-// export * from './basics/ReactCreateElement'
-// export * from './basics/JSX'
-// export * from './basics/RenderingElement'
-// export * from './basics/ComponentsAndProps'
-// export * from './basics/StateAndLifecycle'
-// export * from './basics/HandleEvents'
-// export * from './basics/ConditionalRender'
-// export * from './basics/ListAndKeys'
-// export * from './basics/Forms'
-// export * from './basics/LiftUpState'
-// export * from './basics/Composition'
+export * from './basics/HelloWorld'
+export * from './basics/ReactCreateElement'
+export * from './basics/JSX'
+export * from './basics/RenderingElement'
+export * from './basics/ComponentsAndProps'
+export * from './basics/StateAndLifecycle'
+export * from './basics/HandleEvents'
+export * from './basics/ConditionalRender'
+export * from './basics/ListAndKeys'
+export * from './basics/Forms'
+export * from './basics/LiftUpState'
+export * from './basics/Composition'
 
 /**
  * Advanced
  */
-// export * from './advanced/Accessibility'
-// export * from './advanced/CodeSplitting'
-// export * from './advanced/Context'
-// export * from './advanced/ErrorHandle'
-// export * from './advanced/Fragments'
+export * from './advanced/Accessibility'
+export * from './advanced/CodeSplitting'
+export * from './advanced/Context'
+export * from './advanced/ErrorHandle'
+export * from './advanced/Fragments'
 export * from './advanced/JSXDepth'
